3d45ee48786d7dade5988b477adcbce4a89ba40d
nmesnyankin <nmesnyankin@borlas.ru>
2022-11-28 15:47:26 +0200

    lesson 21 modul

--- src/main/java/ru/mesnyankin/spring/controllers/FirstController.java
+++ src/main/java/ru/mesnyankin/spring/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.mesnyankin.spring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,11 +31,41 @@ public class FirstController {
 
     @GetMapping("/hello") // без required = false, запрос без параметров вернет ошибку
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 
-        System.out.println("Hello, " + name + " " + surname);
+       model.addAttribute("message", "Hello " + name + " " + surname);
 
         return "first/hello";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("a") int a, @RequestParam("b") int b,
+                             @RequestParam("action") String action, Model model) {
+
+        double result;
+
+        switch (action) {
+            case "multiplication":
+                result = a * b;
+                break;
+            case "division":
+                result = a / (double) b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            case "addition":
+                result = a + b;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+
+        model.addAttribute("result", result);
+
+        return "first/calculator";
     }
 
 
